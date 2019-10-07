9815055784c2440882722122af6885e9d6b66afc
Pablo Aravena <paravena74@gmail.com>
2019-10-06 21:06:38 -0300

    first commit

---
+++ src/main.js
@@ -0,0 +1,12 @@
+require("babel-runtime/regenerator");
+require("webpack-hot-middleware/client?reload=true");
+require("./main.css");
+require('./index.html');
+debugger;
+// var a = async (args) => {
+//   const { a, b } = args;
+//   await console.log("Hello from the future!", a, b);
+//   console.log("Done");
+// }
+//
+// a({ a: 1, b: 2 });
